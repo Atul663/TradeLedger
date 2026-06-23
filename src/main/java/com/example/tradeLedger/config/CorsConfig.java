cdbe8ca2160c22c20065892feb66c6f710bb7af9
Atul <atulkumarsethi2001@gmail.com>
2026-06-24 01:02:00 +0530

    nginx setup

--- src/main/java/com/example/tradeLedger/config/CorsConfig.java
+++ src/main/java/com/example/tradeLedger/config/CorsConfig.java
@@ -16,9 +16,9 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOriginPatterns(
                                 "https://trade-pnl-analysis.vercel.app",
-                                "https://proweltconsulting.com",
-                                "https://www.proweltconsulting.com",
-                                "https://api.proweltconsulting.com",
+                                "https://prowfin.proweltconsulting.com",
+                                "https://www.prowfin.proweltconsulting.com",
+                                "https://api.prowfin.proweltconsulting.com",
                                 "http://localhost:5173",
                                 "http://127.0.0.1:5173",
                                 "http://localhost:*",
